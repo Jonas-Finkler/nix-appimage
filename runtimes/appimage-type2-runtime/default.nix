1f61a334b924850b81783122365fc5c4687d260f
Jonas <git@finkler.me>
2024-09-12 09:18:21 +0200

    Use fuse instead of fuse3

--- runtimes/appimage-type2-runtime/default.nix
+++ runtimes/appimage-type2-runtime/default.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub
 , stdenv
-, fuse3
+, fuse
 , pkg-config
 , squashfuse
 , zstd
@@ -18,15 +18,15 @@ let
     hash = "sha256-GR3LMuWMSafQmc2RQyveue3sq+HYBtl+VkcZVYMS0CI=";
   };
 
-  fuse3' = fuse3.overrideAttrs (old: {
-    patches = (old.patches or []) ++ [
-      # this doesn't work -- causes fuse: failed to exec fusermount: Permission denied
-      # "${src}/patches/libfuse/mount.c.diff"
-    ];
-  });
+  # fuse3' = fuse3.overrideAttrs (old: {
+  #   patches = (old.patches or []) ++ [
+  #     # this doesn't work -- causes fuse: failed to exec fusermount: Permission denied
+  #     # "${src}/patches/libfuse/mount.c.diff"
+  #   ];
+  # });
 
   squashfuse' = (squashfuse.override {
-    fuse = fuse3';
+    # fuse = fuse3';
   }).overrideAttrs (old: {
     postInstall = (old.postInstall or "") + ''
       cp *.h -t $out/include/squashfuse/
@@ -41,7 +41,7 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [
-    fuse3'
+    fuse
     squashfuse'
     zstd
     zlib
@@ -55,7 +55,7 @@ stdenv.mkDerivation {
   '';
 
   configurePhase = ''
-    $PKG_CONFIG --cflags fuse3 > cflags
+    $PKG_CONFIG --cflags fuse > cflags
   '';
 
   buildPhase = ''
@@ -63,7 +63,7 @@ stdenv.mkDerivation {
       -D_FILE_OFFSET_BITS=64 -DGIT_COMMIT='"0000000"' \
       $(cat cflags) \
       -std=gnu99 -Os -ffunction-sections -fdata-sections -Wl,--gc-sections -static -Wall -Werror \
-      -lsquashfuse -lsquashfuse_ll -lfuse3 -lzstd -lz -llzma -llz4 -llzo2 \
+      -lsquashfuse -lsquashfuse_ll -lfuse -lzstd -lz -llzma -llz4 -llzo2 \
       -T src/runtime/data_sections.ld
 
     # Add AppImage Type 2 Magic Bytes to runtime
